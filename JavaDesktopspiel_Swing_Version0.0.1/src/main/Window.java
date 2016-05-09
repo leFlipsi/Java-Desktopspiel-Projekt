358002227e682d9b125d7b72c51dc9fa87ad9ac7
PhilippRoehlicke <philipp.roehlicke@googlemail.com>
2016-05-09 18:34:45 +0200

    uii

--- JavaDesktopspiel_Swing_Version0.0.1/src/main/Window.java
+++ JavaDesktopspiel_Swing_Version0.0.1/src/main/Window.java
@@ -1,30 +1,45 @@
 package main;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import konstanten.TextVars;
 
 @SuppressWarnings("serial")
-public class Window extends JFrame{
+public class Window extends JFrame {
 	private int window_width = 1024;
 	private int window_height = 576;
-	
-	public Window(String title){
+	private Menu menu;
+
+	public Window(String title) {
 		this.setResizable(false);
 		this.setTitle(title);
 		this.updateWindowSize();
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setLayout(null);
 		this.setVisible(true);
+		menu = new Menu(this, "start");
 	}
-	
-	public void resetWindowScale(int w, int h){
+
+	public void resetWindowScale(int w, int h) {
 		this.window_width = w;
 		this.window_height = h;
 	}
-	public void updateWindowSize(){
+
+	public void updateWindowSize() {
 		this.setSize(window_width, window_height);
 	}
-	public void keepWindowSize(){
+
+	public void keepWindowSize() {
 		this.window_width = this.getWidth();
 		this.window_height = this.getHeight();
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				Window window = new Window(TextVars.window_title);
+			}
+		});
 	}
 }
